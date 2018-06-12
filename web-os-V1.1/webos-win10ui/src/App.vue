9f545a4b4ea6a5004097ff7143bcf53902e2da14
tangzhichao <alvin198761@163.com>
2018-06-12 19:41:51 +0800

    动画桌面，+ 登录界面

--- web-os-V1.1/webos-win10ui/src/App.vue
+++ web-os-V1.1/webos-win10ui/src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <router-view/>
+    <RootPane></RootPane>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import RootPane from './components/RootPane.vue';
+
+  export default {
+    name: 'App',
+    components: {RootPane}
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
-  width: 100%;
-  height: 100%;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    /*color: #2c3e50;*/
+    /*margin-top: 60px;*/
+    width: 100%;
+    height: 100%;
+  }
 </style>
